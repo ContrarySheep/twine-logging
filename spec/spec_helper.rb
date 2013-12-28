a3421c4ea15acd16fdfce1bf8376abc7c3568291
ContrarySheep <contrarysgeep@gmail.com>
2013-12-28 11:16:22 -0500

    Spec comments

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,11 @@
+# Require the gems needed for testing.
 require 'rubygems'
 require 'rspec'
 require 'net/http'
 require 'yaml'
 
+# Require all files in the lib directory.
 Dir["../../lib/*.rb"].each {|file| require file }
 
+# Load the credentials from the config directory.
 CREDENTIALS = YAML.load(File.read(File.expand_path('../../config/credentials.yml', __FILE__)))
